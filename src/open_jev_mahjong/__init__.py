8d1ce21eb0667c7a029d1fcc743a4ee4eaf6c055
hamakyo <32177429+hamakyo@users.noreply.github.com>
2026-09-21 10:27:07 +0900

    chore: add package init

---
+++ src/open_jev_mahjong/__init__.py
@@ -0,0 +1,5 @@
+"""Open Jev Mahjong: Mortal distillation experiments for riichi mahjong."""
+
+from .schema import LegalActionSpec, build_hard_label_sample
+
+__all__ = ["LegalActionSpec", "build_hard_label_sample"]
